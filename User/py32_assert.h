cc67fa0c4261ea8c6e469425ad085fd79e246e9c
elecboy <elecboy@126.com>
2023-05-13 16:49:52 +0800

    使用DMA测试AT24C32例程代码

--- User/py32_assert.h
+++ User/py32_assert.h
@@ -1,8 +1,7 @@
 /**
   ******************************************************************************
-  * @file    py32f0xx_it.h
-  * @author  MCU Application Team
-  * @brief   This file contains the headers of the interrupt handlers.
+  * @file    py32_assert.h
+  * @brief   PY32 assert file.
   ******************************************************************************
   * @attention
   *
@@ -20,34 +19,41 @@
   ******************************************************************************
   */
 
-
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __PY32F0XX_IT_H
-#define __PY32F0XX_IT_H
+#ifndef __PY32_ASSERT_H
+#define __PY32_ASSERT_H
 
 #ifdef __cplusplus
-extern "C" {
+ extern "C" {
 #endif
 
-/* Private includes ----------------------------------------------------------*/
+#ifdef  USE_FULL_ASSERT
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
-/* Exported functions prototypes ---------------------------------------------*/
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void SVC_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
+/* Includes ------------------------------------------------------------------*/
+#include "stdint.h"
 
-void I2C1_IRQHandler(void);
-void DMA1_Channel1_IRQHandler(void);
-void DMA1_Channel2_3_IRQHandler(void);
+/* Exported macro ------------------------------------------------------------*/
+
+/**
+  * @brief  The assert_param macro is used for function's parameters check.
+  * @param  expr: If expr is false, it calls assert_failed function
+  *         which reports the name of the source file and the source
+  *         line number of the call that failed.
+  *         If expr is true, it returns no value.
+  * @retval None
+  */
+ #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+/* Exported functions ------------------------------------------------------- */
+  void assert_failed(uint8_t* file, uint32_t line);
+#else
+  #define assert_param(expr) ((void)0U)
+#endif /* USE_FULL_ASSERT */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __PY32F0XX_IT_H */
+#endif /* __PY32_ASSERT_H */
 
 /************************ (C) COPYRIGHT Puya *****END OF FILE******************/
